8b07d2688236e159bffeb1e726c73b5e162a12ee
escoladev <escoladevbrasil@gmail.com>
2023-05-13 00:06:25 -0400

    Aula 01 - JPA e Hibernate - Iniciando

--- src/test/java/testeManual/TesteCidadeDao.java
+++ src/test/java/testeManual/TesteCidadeDao.java
@@ -1,5 +1,7 @@
 package testeManual;
 
+import java.util.List;
+
 import dao.CidadeDao;
 import model.Cidade;
 
@@ -18,10 +20,48 @@ public class TesteCidadeDao {
 
     }
 
+    private static void listarDao() {
+
+        CidadeDao cidadeDao = new CidadeDao();
+
+        List<Cidade> cidades= cidadeDao.listar();
+
+        for(Cidade c: cidades){
+            System.out.println(c.getId()+" - "+c.getNome());
+        }
+
+    }
+
+
+    private static void consultarDao(){
+
+        CidadeDao cidadeDao = new CidadeDao();
+
+        Cidade cidade = cidadeDao.consultar(8L);
+
+        System.out.println(cidade.getNome());
+
+    }
+
+    private static void removerDao(){
+
+        CidadeDao cidadeDao = new CidadeDao();
+
+        cidadeDao.remover(8L);
+    }
+
+
+
     public static void main(String[] args) {
 
 
-        inserirDao();
+//        inserirDao();
+//        consultarDao();
+        removerDao();
+        listarDao();
+
 
     }
+
+
 }
