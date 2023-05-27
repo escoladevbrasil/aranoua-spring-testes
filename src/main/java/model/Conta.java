dd18b0b84f967d622053b515ca5e64a3e483557b
escoladev <escoladevbrasil@gmail.com>
2023-05-27 08:47:18 -0400

    Aula05 - ContaTest - Continua

--- src/main/java/model/Conta.java
+++ src/main/java/model/Conta.java
@@ -4,18 +4,40 @@ public class Conta {
 
     private double saldo;
 
+    private boolean ativa;
+
+    public void ativar(){
+        this.ativa = true;
+    }
+
+    public void desativar(){
+        this.ativa = false;
+    }
+
     public double getSaldo() {
         return saldo;
     }
     public void depositar(double valor){
-        saldo += valor;
+
+        if(this.ativa) {
+
+            saldo += valor;
+
+        }else{
+
+            throw new RuntimeException("Conta Inativa! Não é possível depositar.");
+
+        }
+
     }
 
     public void sacar(double valor){
 
-        if(saldo >= valor){
-            saldo -= valor;
-        }
+        if(!this.ativa){ throw new IllegalStateException("Conta Inativa!");}
+
+        if(saldo < valor){ throw new RuntimeException("Saldo menor que o valor de saque!"); }
+
+        saldo -= valor;
 
     }
 
--- src/test/java/model/ContaTest.java
+++ src/test/java/model/ContaTest.java
@@ -1,31 +1,84 @@
 package model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ContaTest {
 
+    private Conta conta;
+
+    private static double valorDeposito;
+    private static double valorSaqueValido;
+    private static double valorSaqueInvalido;
+    private static double valorZerado;
+
+
+    @BeforeAll
+    private static void preparaValores(){
+        valorDeposito = 100.75;
+        valorSaqueValido = 50.00;
+        valorSaqueInvalido = 150.25;
+        valorZerado = 0.00;
+    }
+
+
+    @BeforeEach
+    private void preparaPreCondicao(){
+        this.conta = new Conta();
+        conta.ativar();
+    }
+
     @Test
     public void deveDepositarComValorValido(){
 
-        Conta conta = new Conta();
+        conta.depositar(valorDeposito);
 
-        conta.depositar(100.00);
+        Assertions.assertEquals(valorDeposito,conta.getSaldo());
 
-        Assertions.assertEquals(100.00,conta.getSaldo());
+    }
 
+    @Test
+    public void naoDeveDepositarComContaInativa(){
+
+        conta.desativar();
+
+        Assertions.assertThrows(RuntimeException.class, () -> conta.depositar(valorDeposito));
+
+        Assertions.assertEquals(valorZerado,conta.getSaldo());
     }
 
     @Test
     public void deveSacarComValorMenorQueOSaldo(){
 
-        Conta conta = new Conta();
+        conta.depositar(valorDeposito);
 
-        conta.depositar(100.00);
+        conta.sacar(valorSaqueValido);
 
-        conta.sacar(50.00);
+        Assertions.assertEquals(valorDeposito- valorSaqueValido, conta.getSaldo());
+    }
 
-        Assertions.assertEquals(50.00, conta.getSaldo());
+    @Test
+    public void naoDeveSacarComContaInativa(){
+
+        conta.depositar(valorDeposito);
+
+        conta.desativar();
+
+        Assertions.assertThrows(IllegalStateException.class, () -> conta.sacar(valorSaqueValido));
+
+    }
+
+    @Test
+    public void naoDeveSacarComValorMaiorQueOSaldo(){
+
+        conta.depositar(valorDeposito);
+
+        Assertions.assertThrows(RuntimeException.class, () -> conta.sacar(valorSaqueInvalido) );
+
+        Assertions.assertEquals(valorDeposito, conta.getSaldo());
+
     }
 
 }
